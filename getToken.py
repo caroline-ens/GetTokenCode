cb4e6f7f6bade22d921dc55d072ff9069444f556
Rick <rick@gmail.com>
2026-05-29 00:22:49 -0700

    ddd

--- getToken.py
+++ getToken.py
@@ -2,18 +2,3 @@ import win32cred
 creds = win32cred.CredEnumerate(None, 0)
 for cred in creds:
     target = cred['TargetName']
-
-    if 'github' in target.lower() or 'git:' in target.lower():
-        # print("=" * 50)
-        print("Target:", target)
-
-        blob = cred.get('CredentialBlob')
-
-        if blob:
-            try:
-                if isinstance(blob, bytes):
-                    print("Secret:", blob.decode('utf-16'))
-                else:
-                    print("Secret:", blob)
-            except:
-                print("Raw:", blob)
